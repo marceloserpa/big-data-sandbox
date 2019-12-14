638bdb511fe3c37f0479545a6a3fa129f29a8ac0
marceloserpa <jdev.marcelo@gmail.com>
2019-12-14 20:30:50 -0300

    spark: refactoring to use mapValues on pairRDD

--- spark-pocs/src/main/java/com/marceloserpa/spark/pocs/pairrdd/AirportsNotInUsa.java
+++ spark-pocs/src/main/java/com/marceloserpa/spark/pocs/pairrdd/AirportsNotInUsa.java
@@ -21,9 +21,11 @@ public class AirportsNotInUsa {
 			return new Tuple2<>(splits[1], splits[3]);
 		});
 		
-		JavaPairRDD<String, String> airportsNotInUsa = airportRDD.filter(kv -> !kv.equals("\"United States\""));
+		JavaPairRDD<String, String> airportsNotInUsa = airportRDD.filter(kv -> !kv._2.equals("\"United States\""));
 		
-		airportsNotInUsa.saveAsTextFile("results/airports_not_in_usa");
+		JavaPairRDD<String, String> uppercase = airportsNotInUsa.mapValues(countryName -> countryName.toUpperCase());
+		
+		uppercase.saveAsTextFile("results/airports_not_in_usa");
 	}
 	
 	
