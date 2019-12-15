6c3e12d76b9ef3e3d151e90d6e217c683b256927
marceloserpa <jdev.marcelo@gmail.com>
2019-12-15 16:33:01 -0300

    spark: accumulators

---
+++ spark-pocs/src/main/java/com/marceloserpa/spark/pocs/accumulators/StackOverFlow.java
@@ -0,0 +1,47 @@
+package com.marceloserpa.spark.pocs.accumulators;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.LongAccumulator;
+
+import com.marceloserpa.spark.pocs.Utils;
+
+import scala.Option;
+
+public class StackOverFlow {
+
+	public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("StackOverFlowSurvey").setMaster("local[1]");
+
+        SparkContext sparkContext = new SparkContext(conf);
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkContext);
+
+        final LongAccumulator total = new LongAccumulator();
+        final LongAccumulator missingSalaryMidPoint = new LongAccumulator();
+
+        total.register(sparkContext, Option.apply("total"), false);
+        missingSalaryMidPoint.register(sparkContext, Option.apply("missing salary middle point"), false);
+
+        JavaRDD<String> responseRDD = javaSparkContext.textFile("files/2016-stack-overflow-survey-responses.csv");
+
+        JavaRDD<String> responseFromCanada = responseRDD.filter(response -> {
+            String[] splits = response.split(Utils.COMMA_DELIMITER, -1);
+
+            total.add(1);
+
+            if (splits[14].isEmpty()) {
+                missingSalaryMidPoint.add(1);
+            }
+
+            return splits[2].equals("Canada");
+
+        });
+
+        System.out.println("Count of responses from Canada: " + responseFromCanada.count());
+        System.out.println("Total count of responses: " + total.value());
+        System.out.println("Count of responses missing salary middle point: " + missingSalaryMidPoint.value());
+	}
+	
+}
